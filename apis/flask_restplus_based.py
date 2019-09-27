861d95235dceb65c832f93083eedb8470a88977e
mlefter <m.lefter@lumc.nl>
2019-09-27 11:13:37 +0200

    Update apis to match new hgvs parser.

--- apis/flask_restplus_based.py
+++ apis/flask_restplus_based.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, request
-from flask_restplus import Resource, Api, fields
-from normalizer.normalizer import mutalyzer3, get_reference_models
-from hgvsparser.hgvs_parser import HgvsParser
-from hgvsparser import to_model
+from flask import Flask
+from flask_restplus import Resource, Api
+from normalizer.normalizer import mutalyzer3, get_reference_model
+from mutalyzer_hgvs_parser import parse_description, parse_description_to_model
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Mutalyzer3 API')
@@ -15,21 +14,26 @@ class SyntaxCheck(Resource):
     def get(self, hgvs_description):
         """Takes a variant description as input and checks
         whether its syntax is correct."""
-        parser = HgvsParser()
-        parse_tree = parser.parse(hgvs_description)
-        if parse_tree:
-            return jsonify('OK')
+        try:
+            parse_description(hgvs_description)
+        except Exception:
+            return 'Some error occurred.'
         else:
-            return jsonify('not OK')
+            return 'Correct syntax.'
 
 
 @ns.route("/description_to_model/<string:hgvs_description>")
 class DescriptionToModel(Resource):
     def get(self, hgvs_description):
-        """Converts a variant description to its data model."""
-        parser = HgvsParser()
-        parse_tree = parser.parse(hgvs_description)
-        return jsonify(to_model.convert(parse_tree))
+        """Converts a variant description to its dictionary model."""
+        return parse_description_to_model(hgvs_description)
+
+
+@ns.route("/reference_model/<string:reference_id>")
+class ReferenceModel(Resource):
+    def get(self, reference_id):
+        """Retrieve the reference model."""
+        return get_reference_model(reference_id)
 
 
 @ns.route("/name_check/<string:hgvs_description>")
@@ -37,19 +41,7 @@ class NameCheck(Resource):
     def get(self, hgvs_description):
         """Takes a variant description as input and checks
         whether it is correct."""
-        return jsonify(mutalyzer3(hgvs_description))
-
-
-@ns.route("/reference_models/<string:hgvs_description>")
-class ReferenceModels(Resource):
-    def get(self, hgvs_description):
-        """Takes a variant description as input and retrieves
-        the reference models for all the references present in
-        the description."""
-        parser = HgvsParser()
-        parse_tree = parser.parse(hgvs_description)
-        description_model = to_model.convert(parse_tree)
-        return jsonify(get_reference_models(description_model))
+        return mutalyzer3(hgvs_description)
 
 
 app.run(port=5001, debug=True)
