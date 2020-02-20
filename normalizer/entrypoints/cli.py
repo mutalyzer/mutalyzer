60c8f302735f9984e775d83e618389eacb68c071
mlefter <m.lefter@lumc.nl>
2020-02-20 14:57:35 +0100

    Add restructured files

---
+++ normalizer/entrypoints/cli.py
@@ -0,0 +1,18 @@
+from normalizer.normalizer import mutalyzer3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('description',
+                        help="HGVS variant description to be parsed")
+
+    args = parser.parse_args()
+
+    print(mutalyzer3(args.description))
+
+
+if __name__ == '__main__':
+    main()
