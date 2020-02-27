390e60c70032bd55fdeb71b02ea4cd305cebc604
Mihai <M.Lefter@lumc.nl>
2020-02-27 20:09:24 +0100

    Fix crash due to no features.

--- normalizer/reference.py
+++ normalizer/reference.py
@@ -87,19 +87,20 @@ def get_exon_cds_genomic(selector_id, reference_model):
 
 def get_all_selectors_exon_cds(reference_model):
     output = []
-    for feature in reference_model['features']:
-        if feature['type'] == 'gene' and feature.get('features'):
-            rna_index = 1
-            for sub_feature in feature['features']:
-                gene_id = '{}_v{:03}'.format(feature['id'], rna_index)
-                rna_id = sub_feature['id']
-                exons, cds = get_exons_cds(sub_feature)
-                if exons:
-                    output.append({'exons': exons,
-                                   'cds': cds,
-                                   'id1': gene_id,
-                                   'id2': rna_id})
-                rna_index += 1
+    if reference_model.get('features'):
+        for feature in reference_model['features']:
+            if feature['type'] == 'gene' and feature.get('features'):
+                rna_index = 1
+                for sub_feature in feature['features']:
+                    gene_id = '{}_v{:03}'.format(feature['id'], rna_index)
+                    rna_id = sub_feature['id']
+                    exons, cds = get_exons_cds(sub_feature)
+                    if exons:
+                        output.append({'exons': exons,
+                                       'cds': cds,
+                                       'id1': gene_id,
+                                       'id2': rna_id})
+                    rna_index += 1
     return output
 
 
