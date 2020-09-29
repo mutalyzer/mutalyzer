b41133a39e548d26d88524e1de5f29971f87be79
Mihai <M.Lefter@lumc.nl>
2020-09-29 13:14:26 +0200

    Update tests

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,8 +1,10 @@
-import pytest
-from .test_set import TESTS_ALL
-from normalizer.normalizer import mutalyzer3
 from pathlib import Path
-import json
+
+import pytest
+
+from normalizer.normalizer import normalize
+
+from .test_set import TESTS_ALL
 
 
 def _get_content(relative_location):
@@ -12,12 +14,19 @@ def _get_content(relative_location):
     return content
 
 
-def fetch_annotation(reference_id, reference_type=None):
-    return _get_content("data/" + reference_id + ".gff3"), "gff3", "ncbi"
-
-
-def fetch_sequence(reference_id, reference_source=None):
-    return json.loads(_get_content("data/" + reference_id + ".sequence"))
+def retrieve_raw(
+    reference_id,
+    reference_source=None,
+    reference_type=None,
+    size_off=True,
+    configuration_path=None,
+):
+    if reference_type == "fasta":
+        return _get_content("data/" + reference_id + ".fasta"), "fasta", "ncbi"
+    elif reference_id.startswith("LRG_"):
+        return _get_content("data/" + reference_id), "lrg", "lrg"
+    else:
+        return _get_content("data/" + reference_id + ".gff3"), "gff3", "ncbi"
 
 
 def get_tests(tests):
@@ -29,9 +38,33 @@ def get_tests(tests):
 
 
 @pytest.mark.parametrize("input_description, normalized", get_tests(TESTS_ALL))
-def test_mutalyzer3(input_description, normalized, monkeypatch):
-    monkeypatch.setattr(
-        "mutalyzer_retriever.retriever.fetch_annotations", fetch_annotation
-    )
-    monkeypatch.setattr("mutalyzer_retriever.retriever.fetch_sequence", fetch_sequence)
-    assert mutalyzer3(input_description)["normalized description"] == normalized
+def test_normalizer(input_description, normalized, monkeypatch):
+    monkeypatch.setattr("mutalyzer_retriever.retriever.retrieve_raw", retrieve_raw)
+    assert normalize(input_description) == normalized
+
+
+# def test_normalizer(
+#         monkeypatch,
+#         i_d='LRG_303:g.[105_106del;6681G>C;6883_6884insTTTCGCCCCTTTCGCCCC]',
+#         n_d='LRG_303:g.[108_109del;6681G>C;6868_6869ins[6869_6883;TTT]]',
+# ):
+#     monkeypatch.setattr("mutalyzer_retriever.retriever.retrieve_raw", retrieve_raw)
+#     assert normalize(i_d) == n_d
+
+
+def test_normalizer2(
+    monkeypatch,
+    i_d="NG_017013.2:g.[16985A>T;17011_17012del]",
+    n_d="NG_017013.2:g.[16985A>T;17013_17014del]",
+):
+    monkeypatch.setattr("mutalyzer_retriever.retriever.retrieve_raw", retrieve_raw)
+    assert normalize(i_d) == n_d
+
+
+def test_normalizer3(
+    monkeypatch,
+    i_d="NG_017013.2:g.17415_17417delinsGCG",
+    n_d="NG_017013.2:g.[17415C>G;17417A>G]",
+):
+    monkeypatch.setattr("mutalyzer_retriever.retriever.retrieve_raw", retrieve_raw)
+    assert normalize(i_d) == n_d
