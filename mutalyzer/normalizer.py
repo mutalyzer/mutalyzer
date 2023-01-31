6bce33c2b346cb63e72a6b22c49307deff2633ed
Mihai <M.Lefter@lumc.nl>
2023-01-31 14:42:28 +0100

    Expose the delins model

--- mutalyzer/normalizer.py
+++ mutalyzer/normalizer.py
@@ -17,3 +17,16 @@ def normalize(description, only_variants=False, sequence=None):
     # print("- TOTAL time:", time.time() - t0, "\n\n")
     output = d.output()
     return output
+
+
+def delins_model(description, only_variants=False, sequence=None):
+    d = Description(
+        description=description,
+        only_variants=only_variants,
+        sequence=sequence,
+    )
+    d.to_delins()
+    output = d.output()
+    if d.delins_model:
+        output["delins_model"] = d.delins_model
+    return output
