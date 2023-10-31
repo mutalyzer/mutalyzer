508a9f74a7a0ea8483f864b757614fe1a328ca5f
Mihai Lefter <mihai.lefter@gmail.com>
2023-10-31 16:07:37 +0100

    Cleaning

--- mutalyzer/normalizer.py
+++ mutalyzer/normalizer.py
@@ -1,21 +1,21 @@
-import time
+"""Interfaces to obtain the normalized (canonical) variant representations or
+the delins model of an input description."""
 
 from .description import Description
 from .util import construct_sequence
 
 
 def normalize(description, only_variants=False, sequence=None):
-    # t0 = time.time()
+    """
+    Obtain the normalized (canonical) variant representation.
+    """
     d = Description(
         description=description,
         only_variants=only_variants,
         sequence=sequence,
     )
     d.normalize()
-    t = time.time()
     d.get_chromosomal_descriptions()
-    # print("get_chromosomal_description:", time.time() - t)
-    # print("- TOTAL time:", time.time() - t0, "\n\n")
     output = d.output()
     return output
 
