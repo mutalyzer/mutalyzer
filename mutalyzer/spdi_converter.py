68618e64dc5c09f99dc5c5765a2f6d9a94c4580e
Mihai Lefter <mihai.lefter@gmail.com>
2024-05-28 11:48:56 +0200

    Support assembly ids (#95)

--- mutalyzer/spdi_converter.py
+++ mutalyzer/spdi_converter.py
@@ -3,13 +3,13 @@ from mutalyzer_mutator import mutate
 from mutalyzer_spdi_parser.convert import to_hgvs_internal_model as spdi_to_hgvs
 
 from .converter.to_hgvs_coordinates import to_hgvs_locations
-from .converter.to_internal_indexing import variants_to_internal_indexing
 from .converter.variants_de_to_hgvs import de_to_hgvs
-from .description import Description
 from .description_model import model_to_string
-from .errors import out_of_boundary_greater, sequence_mismatch
+from .errors import out_of_boundary_greater, sequence_mismatch, reference_not_retrieved
 from .reference import get_coordinate_system_from_reference, retrieve_reference
 from .util import get_end, get_start
+from mutalyzer_retriever.reference import get_assembly_id, get_assembly_chromosome_accession
+from mutalyzer_retriever.retriever import get_chromosome_from_selector
 
 
 def _errors_check(variant, ref_seq):
@@ -41,6 +41,26 @@ def _errors_check(variant, ref_seq):
     return errors
 
 
+def _get_ids(model):
+    r_id = model["reference"]["id"]
+    s_id = None
+    if model["reference"].get("selector") and model["reference"]["selector"].get("id"):
+        s_id = model["reference"]["selector"]["id"]
+
+    a_id = get_assembly_id(r_id)
+    if a_id:
+        chromosome_id = get_assembly_chromosome_accession(r_id, s_id)
+        if chromosome_id:
+            r_id = chromosome_id
+            s_id = None
+        else:
+            chromosome_id = get_chromosome_from_selector(a_id, s_id)
+            if chromosome_id:
+                r_id = chromosome_id
+                s_id = None
+    return r_id, s_id
+
+
 def spdi_converter(description=None, description_model=None):
     if description:
         model = spdi_to_hgvs(description)
@@ -49,8 +69,11 @@ def spdi_converter(description=None, description_model=None):
     else:
         return
 
-    r_m = retrieve_reference(model["reference"]["id"])[0]
-    print(r_m)
+    r_id, s_id = _get_ids(model)
+    r_m = retrieve_reference(r_id, s_id)[0]
+
+    if r_m is None:
+        return {"errors": [reference_not_retrieved(r_id, ["reference", "id"])]}
 
     errors = _errors_check(model["variants"][0], r_m["sequence"]["seq"])
     if errors:
@@ -63,15 +86,15 @@ def spdi_converter(description=None, description_model=None):
         "variants": de_to_hgvs(
             d_v, {"reference": r_m["sequence"]["seq"], "observed": obs_seq}
         ),
-        "reference": {"id": model["reference"]["id"]},
+        "reference": {"id": r_id},
         "coordinate_system": "i",
     }
 
     c_s = get_coordinate_system_from_reference(r_m)
     if c_s in ["c", "n"]:
-        n_m = to_hgvs_locations(d_h_m, {model["reference"]["id"]: r_m}, c_s, model["reference"]["id"])
+        n_m = to_hgvs_locations(d_h_m, {r_id: r_m}, c_s, r_id)
     else:
-        n_m = to_hgvs_locations(d_h_m, {model["reference"]["id"]: r_m}, c_s)
+        n_m = to_hgvs_locations(d_h_m, {r_id: r_m}, c_s)
     n_d = model_to_string(n_m)
 
     return {"input_model": model, "normalized_description": n_d}
