c3fd26b2d337a6f9178d2f283e1b62cd20be6ff9
Xiaoyun Liu <x.liu@lumc.nl>
2025-12-17 15:47:01 +0100

    Fetch overlap/annotated genes instead of transcripts.

--- mutalyzer/equivalent.py
+++ mutalyzer/equivalent.py
@@ -8,6 +8,9 @@ from algebra.lcs.lcs_graph import LCSgraph
 from algebra.extractor.extractor import canonical
 
 
+import pprint
+
+
 def is_mane(feature_model):
     """Check if a feature model is a MANE Select transcript model."""
     if (
@@ -37,36 +40,53 @@ def overlap_mane_selectors(reference_id, start, end):
     return mane_selectors
 
 
-def annotated_transcripts(reference_id):
-    """Return all annotated transcripts for a given reference."""
+def annotated_genes(reference_id):
+    """Return all annotated genes for a given reference."""
     reference_model, found = get_reference_model(reference_id)
     if not found:
         return list()
-    annotated_transcripts = list()
+    annotated_genes = list()
     reference_annotations = reference_model.get("annotations", {})
     for feature in yield_feature_models(reference_annotations):
-        if "rna" in feature.get("type").lower():
-            annotated_transcripts.append(feature.get("id"))
-    return annotated_transcripts
+        if "gene" in feature.get("type").lower():
+            annotated_genes.append(feature.get("id"))
+    return annotated_genes
 
-def overlap_transcripts(reference_id, start, end):
-    """"Return overlapping transcripts for a given reference and position."""
+def overlap_genes(reference_id, start, end):
+    """"Return overlapping genes for a given reference and position."""
     # retrieve reference model using reference id
     # loop over the features of the model and check if there are features of interest overlapping the position
-    #   - feature type: check if is transcript
+    #   - feature type: check if is gene
     #   - location range: contains input
-    # retrun: a list of overlap transcripts
+    # retrun: a list of overlap genes
 
     reference_model, found = get_reference_model(reference_id)
     if not found:
         return list()
-    overlap_transcripts = list()
+    overlap_genes = list()
     reference_annotations = reference_model.get("annotations", {})
     for feature in yield_feature_models(reference_annotations):
-        if "rna" in feature.get("type").lower() and is_overlap(feature, start, end):
-            overlap_transcripts.append(feature.get("id"))
-    return overlap_transcripts
+        if "gene" in feature.get("type").lower() and is_overlap(feature, start, end):
+            overlap_genes.append(feature.get("id"))
+    return overlap_genes
 
+
+def gene_to_transcripts(reference_id: str, gene_symbol: str):
+    """Return all transcripts for a given gene in a reference."""
+    reference_model, found = get_reference_model(reference_id)
+    if not found:
+        return list()
+    transcripts = list()
+    reference_annotations = reference_model.get("annotations", {})
+    for feature in yield_feature_models(reference_annotations):
+        if (
+            "gene" == feature.get("type")
+            and gene_symbol.lower() == feature.get("id",[]).lower()
+        ):
+            for sub_feature in yield_feature_models(feature):
+                if "rna" in sub_feature.get("type").lower():
+                    transcripts.append(sub_feature.get("id"))
+    return transcripts
 
 def get_normalized_model(description):
     d = Description(
@@ -74,7 +94,7 @@ def get_normalized_model(description):
         only_variants=False,
         sequence=None,
     )
-    d.normalize(include_extras=False)
+    d.normalize(include_extras=True)
 
     if d.errors:
         return d
@@ -110,37 +130,66 @@ def get_canonical_variants_boundaries(description):
     return boundaries
 
 
-def convert_description(description, selectors):
+def transcript_to_gene(transcript_id: str):
+    """Return the gene symbol for a given transcript id."""
+    # retrieve reference model
+    # extract gene feature, return gene symbol
+    transcript_model, found = get_reference_model(transcript_id)
+    if not found:
+        return None
+    transcript_annotations = transcript_model.get("annotations", {})
+    for feature in yield_feature_models(transcript_annotations):
+        if "gene" in feature.get("type").lower():
+            return feature.get("id")
+    return None
+
+
+def convert_description(description, selector_id):
     """Convert a given variant description to a transcript specific description."""
+    #TODO:
+        # -Discuss if the description should be noramalized or not, now it is normalized first
+        # -Add support for transcript descriptison as input (e,g, NM_001127208.3:c.100del)
+        #  via chromosomal description?
+
+    # -normalize the description
+    # -check if selector is in the affected genes
     d = get_normalized_model(description)
     if d.errors:
         return d
-    reference_id = get_reference_id(d.corrected_model)
-    all_transcripts = annotated_transcripts(reference_id)
-    missing_transcripts = list(selectors) - all_transcripts
-    if missing_transcripts:
-        return {
-            "errors": [{"details": f"Transcript(s) not found: {', '.join(missing_transcripts)}"}],
-            "source": "conversion"
-        }
-    converted_descriptions = []
 
+    gene = transcript_to_gene(selector_id)
+    if not gene:
+        raise ValueError(f"Cannot find annotated gene for transcript {selector_id}.")
+
+    affected_genes = set()
+    boundaries = get_canonical_variants_boundaries(description)
+    for start, end in boundaries:
+        affected_gene = overlap_genes(
+            reference_id=get_reference_id(d.corrected_model),
+            start=start,
+            end=end,
+        )
+        affected_genes.update(affected_gene)
+    if gene not in affected_genes:
+        raise ValueError(f"Selector transcript {selector_id} does not belong to affected genes {affected_genes}.")
+
+    converted_descriptions = []
     if d.de_hgvs_internal_indexing_model:
         from_model = d.de_hgvs_internal_indexing_model
     else:
         from_model = None
-    for selector in selectors:
-        try:
-            converted_model = to_hgvs_locations(
-                model=from_model,
-                references=d.references,
-                to_coordinate_system="c",
-                to_selector_id=selector,
-                degenerate=True,
-            )
-        except Exception as e:
-            return {"errors": [{"details": str(e)}], "source": "conversion"}
-        converted_descriptions.append(model_to_string(converted_model))
+
+    try:
+        converted_model = to_hgvs_locations(
+            model=from_model,
+            references=d.references,
+            to_coordinate_system="c",
+            to_selector_id=selector_id,
+            degenerate=True,
+        )
+    except Exception as e:
+        return {"errors": [{"details": str(e)}], "source": "conversion"}
+    converted_descriptions.append(model_to_string(converted_model))
     return converted_descriptions
 
 
@@ -149,16 +198,16 @@ if __name__ == "__main__":
         description="Generate equivalent variant descriptions for a transcript."
     )
 
-    parser.append_argument(
+    parser.add_argument(
         "reference",
         help="A reference sequence accession (e.g., 'NC_000023.11')"
     )
-    parser.append_argument(
+    parser.add_argument(
         "start",
         type=int,
         help="A location point at the start of the mutation"
     )
-    parser.append_argument(
+    parser.add_argument(
         "end",
         type=int,
         help="A location point at the end of the mutation"
@@ -166,8 +215,8 @@ if __name__ == "__main__":
 
 
     args = parser.parse_args()
-    print(convert_description("NC_000011.10(NM_003002.4):c.274G>T", ["NM_003002.4", "NM_001276504.2"]))
+    print(convert_description("NC_000004.12(XM_047415843.1):c.100del", "NM_001127208.3"))
 
-    # print(overlap_transcripts(args.reference, args.start, args.end))
-    print(annotated_transcripts(args.reference))
+    print(overlap_genes(args.reference, args.start, args.end))
+    print(gene_to_transcripts(args.reference, "SDHD"))
     # print(overlap_mane_selectors(args.reference, args.start, args.end))
